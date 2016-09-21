1b01d6742707c1d282fa84cba1a9575b787962c8
duxingcai <xingcaidu@yahoo.com>
2016-09-21 20:57:45 +0800

    update Item12

---
+++ Item12/app/src/main/java/com/example/duduxing/item12/Fragment1.java
@@ -0,0 +1,18 @@
+package com.example.duduxing.item12;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by uniqu on 2016/9/10.
+ */
+public class Fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_view1, container, false);
+    }
+}
---
+++ Item12/app/src/main/java/com/example/duduxing/item12/FragmentColumnActivity.java
@@ -0,0 +1,47 @@
+package com.example.duduxing.item12;
+//理解Fragment，掌握Fragment的使用并编程实现简单单词本
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FragmentColumnActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView tab1, tab2, tab3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_column);
+
+
+        tab1 = (TextView) findViewById(R.id.tab1);
+        tab2 = (TextView) findViewById(R.id.tab2);
+        tab3 = (TextView) findViewById(R.id.tab3);
+
+        tab1.setOnClickListener(this);
+        tab2.setOnClickListener(this);
+        tab3.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        android.app.FragmentManager fm = getFragmentManager();
+        android.app.FragmentTransaction ft = fm.beginTransaction();
+
+        switch (view.getId()) {
+            case R.id.tab1:
+                ft.replace(R.id.content, new Fragment1());
+                break;
+            case R.id.tab2:
+                ft.replace(R.id.content, new Fragment2());
+                break;
+            case R.id.tab3:
+                ft.replace(R.id.content, new Fragment3());
+                break;
+            default:
+                break;
+        }
+
+        ft.commit();
+    }
+}
