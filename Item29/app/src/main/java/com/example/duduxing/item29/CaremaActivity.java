b660040a385f48645a2e80974f2e6caf47b4fccb
duxingcai <xingcaidu@yahoo.com>
2016-09-27 20:42:41 +0800

    update Item29

---
+++ Item29/app/src/main/java/com/example/duduxing/item29/CaremaActivity.java
@@ -0,0 +1,42 @@
+package com.example.duduxing.item29;
+//编程实现拍照、把照片保存为文件等
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CaremaActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carema);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, Activity.DEFAULT_KEYS_DIALER);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        Bitmap bitmap = (Bitmap) bundle.get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+}
