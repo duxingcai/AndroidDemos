0172e874597c5e17a88264e3f433e1a16dcdd8d8
duxingcai <xingcaidu@yahoo.com>
2016-09-30 13:21:21 +0800

    update Item16

---
+++ Item16/app/src/main/java/com/example/duduxing/item16/ImplicitActivity.java
@@ -0,0 +1,28 @@
+package com.example.duduxing.item16;
+
+//掌握使用Intent隐式启动的方法,进一步熟悉Intent Filter
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ImplicitActivity extends AppCompatActivity {
+    protected Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.duduxing.item16.SecondActivity");
+                startActivity(intent);
+            }
+        });
+    }
+}
